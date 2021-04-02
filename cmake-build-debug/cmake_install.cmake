8e84526da499bbc73cf55a38cf34f2fef51d65ec
Johan FrediN <johanfredin@fastmail.coM>
2021-04-02 04:45:45 +0200

    Renamed project

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/lowrider/CLionProjects/playground
+# Install script for directory: /mnt/c/Users/lowrider/CLionProjects/badjson
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/lowrider/CLionProjects/playground/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/lowrider/CLionProjects/badjson/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
